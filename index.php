76640024c82ad2414dc5423025165ad61db543c1
Robert Cox <rcox18@mail.greenriver.edu>
2020-01-19 03:15:41 -0500

    style touch ups

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-/*
+/**
  * @author     Robert Cox
  * @version    1.0.0
  * @link       http://rcox.greenriverdev.com/IT328/chicken
- * @since      1/15/2020
+ * @since      1/19/2020
  */
 
 ini_set('display_errors', 1);
